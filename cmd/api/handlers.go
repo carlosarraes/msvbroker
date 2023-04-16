e3a66877168abe6e262696cc6525dd2ea74c69f4
carraes <carraeshb@gmail.com>
2023-04-16 00:23:31 -0300

    feat: basic broker setup

---
+++ cmd/api/handlers.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type jsonResponse struct {
+	Error   bool   `json:"error"`
+	Message string `json:"message"`
+	Data    any    `json:"data,omitempty"`
+}
+
+func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
+	payload := jsonResponse{
+		Error:   false,
+		Message: "Broker",
+	}
+
+	out, _ := json.MarshalIndent(payload, "", "\t")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write(out)
+	if err != nil {
+		log.Panic(err)
+	}
+}
